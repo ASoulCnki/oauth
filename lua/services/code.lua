88bae9dbd6db1eb9f277aa8c17b0e565e16e5453
ch3cknull <ch3cknull@outlook.com>
2021-12-10 16:57:01 +0800

    perf(code): change json encode to string format

--- lua/services/code.lua
+++ lua/services/code.lua
@@ -5,7 +5,7 @@ local config = require("config.config")
 
 local function NotAuthorized(err)
     ngx.status = ngx.HTTP_UNAUTHORIZED
-    ngx.say(string.format([[{"code": 401,"message": "%s"}]], err))
+    ngx.say(string.format([[{"code":401,"message":"%s"}]], err))
     ngx.exit(ngx.HTTP_UNAUTHORIZED)
 end
 -- auth start
@@ -18,9 +18,5 @@ if not token then
     NotAuthorized(err)
 end
 
-local response = {
-    code = 0,
-    message = "ok",
-    data = json.decode(token)
-}
-ngx.say(json.encode(response))
+local resp_template = [[{"code":0,"message":"ok","data":%s}]]
+ngx.say(string.format(resp_template, token))
